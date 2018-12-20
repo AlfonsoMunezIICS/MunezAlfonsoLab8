515150d2f7278b34df191258e50f1ca17dc7ac01
AlfonsoMunezIICS <2015081677@ust-ics.mygbiz.com>
2018-12-20 23:56:45 +0800

    Initial commit

---
+++ app/src/main/java/com/munez/alfonso/Person.java
@@ -0,0 +1,22 @@
+package com.munez.alfonso;
+
+public class Person {
+    String name, age, gender;
+
+    public Person(String name, String age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
